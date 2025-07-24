5b9af348f50a1d535409f61eb851b06d43bb9106
polagouni <macbookpro@Macbooks-MacBook-Pro.local>
2025-07-24 15:27:56 -0500

    first commit

---
+++ src/com/class1/Practice1.java
@@ -0,0 +1,12 @@
+package com.class1;
+
+import java.util.Scanner;
+public class Practice1 {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       int a = scanner.nextInt();
+       int b = scanner.nextInt();
+       
+       System.out.print(a+""+b);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author macbookpro
+ *
+ */
+module DSA {
+}
